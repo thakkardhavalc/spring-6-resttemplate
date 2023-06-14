856705b656b58dca66a4e164275de23df54a83fe
thakkardhavalc <thakkardhavalc@gmail.com>
2023-06-14 16:18:45 -0600

    first commit

---
+++ src/main/java/guru/springframework/spring6resttemplate/Spring6resttemplateApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6resttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6resttemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6resttemplateApplication.class, args);
+	}
+
+}
