f796800b7ec044dddbe01d570af103f1e6d1868c
thakkardhavalc <thakkardhavalc@gmail.com>
2023-06-21 15:27:42 -0600

    assignment to perform queryParameters on beerStyle, pageNumber and pageSize

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClient.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClient.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
 import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
+import guru.springframework.spring6resttemplate.model.BeerStyle;
 import org.springframework.data.domain.Page;
 
 /**
@@ -9,5 +10,8 @@ import org.springframework.data.domain.Page;
  */
 public interface BeerClient {
 
-    Page<BeerDTO> listBeers(String beerName);
+    Page<BeerDTO> listBeers();
+
+    Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory,
+                            Integer pageNumber, Integer pageSize);
 }
--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,8 +1,11 @@
 package guru.springframework.spring6resttemplate.client;
 
+import guru.springframework.spring6resttemplate.model.BeerDTO;
 import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
+import guru.springframework.spring6resttemplate.model.BeerStyle;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -20,13 +23,35 @@ public class BeerClientImpl implements BeerClient {
     private static final String GET_BEER_PATH = "/api/v1/beer";
 
     @Override
-    public BeerDTOPageImpl listBeers(String beerName) {
+    public Page<BeerDTO> listBeers() {
+        return this.listBeers(null, null, null, null, null);
+    }
+
+    @Override
+    public BeerDTOPageImpl listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory,
+                                     Integer pageNumber, Integer pageSize) {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
         UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath(GET_BEER_PATH);
 
         if (beerName != null) {
             uriComponentsBuilder.queryParam("beerName", beerName);
+        }
+
+        if (beerStyle != null) {
+            uriComponentsBuilder.queryParam("beerStyle", beerStyle);
+        }
+
+        if (showInventory != null) {
+            uriComponentsBuilder.queryParam("showInventory", showInventory);
+        }
+
+        if (pageNumber != null) {
+            uriComponentsBuilder.queryParam("pageNumber", pageNumber);
+        }
+
+        if (pageSize != null) {
+            uriComponentsBuilder.queryParam("pageSize", pageSize);
         }
 
         ResponseEntity<BeerDTOPageImpl> stringResponse =
