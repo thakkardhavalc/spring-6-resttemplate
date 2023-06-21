2a9b205f7a75fd098e85b4849cf1faeb0e22106c
thakkardhavalc <thakkardhavalc@gmail.com>
2023-06-21 15:36:48 -0600

    added test case to perform getBeerById

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClient.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClient.java
@@ -1,9 +1,10 @@
 package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
-import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
 import guru.springframework.spring6resttemplate.model.BeerStyle;
 import org.springframework.data.domain.Page;
+
+import java.util.UUID;
 
 /**
  * Created By dhaval on 2023-06-14
@@ -14,4 +15,6 @@ public interface BeerClient {
 
     Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory,
                             Integer pageNumber, Integer pageSize);
+
+    BeerDTO getBeerById(UUID beerId);
 }
--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,8 +1,12 @@
 package guru.springframework.spring6resttemplate.client;
 
+import guru.springframework.spring6resttemplate.model.BeerDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created By dhaval on 2023-06-14
@@ -12,6 +16,18 @@ class BeerClientImplTest {
 
     @Autowired
     BeerClientImpl beerClient;
+
+    @Test
+    void getBeerById() {
+
+        Page<BeerDTO> beerDTOS = beerClient.listBeers();
+
+        BeerDTO dto = beerDTOS.getContent().get(0);
+
+        BeerDTO byId = beerClient.getBeerById(dto.getId());
+
+        assertThat(byId).isNotNull();
+    }
 
     @Test
     void listBeersNoBeerName() {
